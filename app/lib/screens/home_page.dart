f3755bda33a6666872f97beadce6b8a01c53c53e
NayaRPL2 <ailsanayaelvani@gmail.com>
2026-07-24 13:55:29 +0700

    update: homepage.dart file idk this code wwkkwk

--- app/lib/screens/home_page.dart
+++ app/lib/screens/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:app/model/product.dart';
+import 'package:app/service/api_service.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,8 +10,54 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final ApiService api = ApiService();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Product List"),
+        centerTitle: true,
+      ),
+
+      body: FutureBuilder<List<Product>>(
+        future: api.getProducts(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if(snapshot.hasError) {
+            return Center(
+              child: Text(snapshot.error.toString()),
+            );
+          }
+
+          if(!snapshot.hasData) {
+            return const Center(
+              child: Text("Data is empty"),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              return ProductCard(
+                product:snapshot.data![index]
+              );
+            },
+          );
+        },
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
--- app/lib/service/api_service.dart
+++ app/lib/service/api_service.dart
@@ -1,3 +1,20 @@
+import 'dart:convert';
+
+import 'package:app/model/product.dart';
+import 'package:http/http.dart' as http;
+
 class ApiService {
-  static const String baseUrl = "http://127.0.0.1:8000";
+  static const String baseUrl = "http://127.0.0.1:8000/api";
+  Future<List<Product>>getProducts() async {
+    final response = await http.get(
+      Uri.parse("${baseUrl}/products"),
+    );
+    
+    if (response.statusCode == 200) {
+      List jsonData=jsonDecode(response.body);
+      return jsonData.map((e) => Product.fromJson(e)).toList();
+    } else {
+      throw Exception("Get all data failed");
+    }
+  }
 }
